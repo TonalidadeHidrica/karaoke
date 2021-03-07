b1ae1f2cf0a51702cf7712189f4a4cea7945c5f9
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-03-07 18:21:51 +0900

    Implement custom data type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use druid::im::Vector;
 use druid::kurbo::Line;
 use druid::AppLauncher;
 use druid::Color;
+use druid::Data;
 use druid::PlatformError;
 use druid::RenderContext;
 use druid::Widget;
@@ -8,8 +10,8 @@ use druid::WindowDesc;
 use thiserror::Error;
 
 fn main() -> Result<(), EditorError> {
-    let window = WindowDesc::new(|| ScoreEditor);
-    AppLauncher::with_window(window).launch(())?;
+    let window = WindowDesc::new(|| ScoreEditor {});
+    AppLauncher::with_window(window).launch(Score::default())?;
 
     Ok(())
 }
@@ -20,14 +22,22 @@ pub enum EditorError {
     DruidError(#[from] PlatformError),
 }
 
-struct ScoreEditor;
+#[derive(Clone, Default, Data)]
+struct Score {
+    elements: Vector<ScoreElement>,
+}
 
-impl Widget<()> for ScoreEditor {
+#[derive(Clone, PartialEq, Data)]
+enum ScoreElement {}
+
+struct ScoreEditor {}
+
+impl Widget<Score> for ScoreEditor {
     fn event(
         &mut self,
         _ctx: &mut druid::EventCtx,
         _event: &druid::Event,
-        _data: &mut (),
+        _data: &mut Score,
         _env: &druid::Env,
     ) {
         // dbg!(event);
@@ -37,7 +47,7 @@ impl Widget<()> for ScoreEditor {
         &mut self,
         _ctx: &mut druid::LifeCycleCtx,
         _event: &druid::LifeCycle,
-        _data: &(),
+        _data: &Score,
         _env: &druid::Env,
     ) {
     }
@@ -45,8 +55,8 @@ impl Widget<()> for ScoreEditor {
     fn update(
         &mut self,
         _ctx: &mut druid::UpdateCtx,
-        _old_data: &(),
-        _data: &(),
+        _old_data: &Score,
+        _data: &Score,
         _env: &druid::Env,
     ) {
     }
@@ -55,14 +65,14 @@ impl Widget<()> for ScoreEditor {
         &mut self,
         _ctx: &mut druid::LayoutCtx,
         bc: &druid::BoxConstraints,
-        _data: &(),
+        _data: &Score,
         _env: &druid::Env,
     ) -> druid::Size {
         // TODO example says that we have to check if constraints is bounded
         bc.max()
     }
 
-    fn paint(&mut self, ctx: &mut druid::PaintCtx, _data: &(), _env: &druid::Env) {
+    fn paint(&mut self, ctx: &mut druid::PaintCtx, _data: &Score, _env: &druid::Env) {
         let line = Line::new((10.0, 10.0), (20.0, 20.0));
         ctx.stroke(line, &Color::WHITE, 1.0);
     }
