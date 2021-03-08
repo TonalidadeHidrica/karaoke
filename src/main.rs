abfe2f4770d7f12fb7ebd244ea5348ac591018f2
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-03-08 18:58:50 +0900

    Draw measure lines, take 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use druid::keyboard_types::Key;
+use druid::kurbo::Line;
 use druid::AppLauncher;
 use druid::Color;
 use druid::Event;
@@ -9,12 +10,16 @@ use druid::RenderContext;
 use druid::Size;
 use druid::Widget;
 use druid::WindowDesc;
+use itertools::iterate;
 use karaoke::schema::iterate_elements;
+use karaoke::schema::iterate_measures;
+use karaoke::schema::BeatLength;
 use karaoke::schema::BeatPosition;
 use karaoke::schema::Score;
 use karaoke::schema::ScoreElement;
 use karaoke::schema::ScoreElementKind;
 use num::BigRational;
+use num::ToPrimitive;
 use num::Zero;
 use thiserror::Error;
 
@@ -39,7 +44,7 @@ struct ScoreEditor {
 impl Default for ScoreEditor {
     fn default() -> Self {
         ScoreEditor {
-            cursor_position: BigRational::zero().into(),
+            cursor_position: BeatPosition::zero(),
         }
     }
 }
@@ -82,6 +87,17 @@ impl Widget<Score> for ScoreEditor {
                     score.elements.pop_back();
                     ctx.request_paint();
                 }
+                Key::ArrowLeft => {
+                    self.cursor_position -= BeatLength::one();
+                    if self.cursor_position < BeatPosition::zero() {
+                        self.cursor_position = BeatPosition::zero();
+                    }
+                    ctx.request_paint();
+                }
+                Key::ArrowRight => {
+                    self.cursor_position += BeatLength::one();
+                    ctx.request_paint();
+                }
                 _ => {}
             },
             _ => {}
@@ -117,12 +133,56 @@ impl Widget<Score> for ScoreEditor {
         bc.max()
     }
 
+    #[allow(unreachable_code)]
     fn paint(&mut self, ctx: &mut druid::PaintCtx, data: &Score, _env: &druid::Env) {
         let insets = Insets::uniform(-8.0);
         let rect = ctx.size().to_rect().inset(insets);
-        let beat_width = 15.0;
-        let line_height = 20.0;
+        let beat_width = 60.0;
+        let line_height = 15.0;
         let note_height = 12.0;
+        let line_margin = 5.0;
+
+        {
+            let mut y = rect.min_y();
+            let mut left_beat: BeatPosition = BeatPosition::zero();
+            let get_x = |length: BeatLength| rect.min_x() + length.0.to_f64().unwrap() * beat_width;
+            for (beat_start, beat_end) in iterate_measures(&data.measure_lengths) {
+                if &beat_end - &left_beat > BeatLength::from(BigRational::from_integer(16.into())) {
+                    let x = get_x(&beat_start - &left_beat);
+                    let line = Line::new((x, y), (x, y + line_height));
+                    ctx.stroke(line, &Color::GRAY, 2.0);
+
+                    left_beat = beat_start.clone();
+                    y += line_height + line_margin;
+                }
+
+                let x = get_x(&beat_start - &left_beat);
+                let line = Line::new((x, y), (x, y + line_height));
+                ctx.stroke(line, &Color::GRAY, 2.0);
+
+                if &beat_start < &self.cursor_position {
+                    for b in iterate(beat_start.clone(), |x| x + &BeatLength::one())
+                        .skip(1)
+                        .take_while(|b| b < &beat_end)
+                    {
+                        let x = get_x(&b - &left_beat);
+                        let line = Line::new((x, y + 2.0), (x, y + line_height));
+                        ctx.stroke(line, &Color::GRAY, 1.0);
+                    }
+                }
+
+                if (&beat_start..&beat_end).contains(&&self.cursor_position) {
+                    dbg!(&self.cursor_position, &left_beat);
+                    let x = get_x(&self.cursor_position - &left_beat);
+                    let line = Line::new((x, y), (x, y + line_height));
+                    ctx.stroke(line, &Color::GREEN, 3.0);
+                }
+
+                if &self.cursor_position <= &beat_start {
+                    break;
+                }
+            }
+        }
 
         for (i, j) in iterate_elements(data.elements.iter()) {
             let rect = Size::new((j - i) as f64 * beat_width, note_height)
--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,9 @@
 use std::borrow::Borrow;
+use std::iter;
+use std::ops::Add;
+use std::ops::AddAssign;
+use std::ops::Sub;
+use std::ops::SubAssign;
 
 use derive_more::From;
 use druid::im::OrdMap;
@@ -6,11 +11,97 @@ use druid::im::Vector;
 use druid::Data;
 use itertools::Itertools;
 use num::rational::BigRational;
+use num::One;
+use num::Zero;
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Data, From)]
-pub struct BeatPosition(#[data(same_fn = "PartialEq::eq")] BigRational);
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Data, From)]
-pub struct BeatLength(#[data(same_fn = "PartialEq::eq")] BigRational);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, From, Debug, Data)]
+pub struct BeatPosition(#[data(same_fn = "PartialEq::eq")] pub BigRational);
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, From, Debug, Data)]
+pub struct BeatLength(#[data(same_fn = "PartialEq::eq")] pub BigRational);
+
+impl BeatPosition {
+    pub fn zero() -> Self {
+        Self(BigRational::zero())
+    }
+}
+
+impl BeatLength {
+    pub fn one() -> Self {
+        Self(BigRational::one())
+    }
+}
+
+impl Add<BeatLength> for BeatPosition {
+    type Output = BeatPosition;
+
+    fn add(self, rhs: BeatLength) -> Self::Output {
+        (self.0 + rhs.0).into()
+    }
+}
+
+impl Add<&BeatLength> for &BeatPosition {
+    type Output = BeatPosition;
+
+    fn add(self, rhs: &BeatLength) -> Self::Output {
+        self.clone() + rhs.clone()
+    }
+}
+
+impl AddAssign<BeatLength> for BeatPosition {
+    fn add_assign(&mut self, rhs: BeatLength) {
+        self.0 += rhs.0
+    }
+}
+
+impl AddAssign<&BeatLength> for BeatPosition {
+    fn add_assign(&mut self, rhs: &BeatLength) {
+        self.0 += rhs.0.clone()
+    }
+}
+
+impl Sub<BeatLength> for BeatPosition {
+    type Output = BeatPosition;
+
+    fn sub(self, rhs: BeatLength) -> Self::Output {
+        (self.0 - rhs.0).into()
+    }
+}
+
+impl Sub<&BeatLength> for &BeatPosition {
+    type Output = BeatPosition;
+
+    fn sub(self, rhs: &BeatLength) -> Self::Output {
+        self.clone() - rhs.clone()
+    }
+}
+
+impl SubAssign<BeatLength> for BeatPosition {
+    fn sub_assign(&mut self, rhs: BeatLength) {
+        self.0 -= rhs.0
+    }
+}
+
+impl SubAssign<&BeatLength> for BeatPosition {
+    fn sub_assign(&mut self, rhs: &BeatLength) {
+        self.0 -= rhs.0.clone()
+    }
+}
+
+impl Sub<BeatPosition> for BeatPosition {
+    type Output = BeatLength;
+
+    fn sub(self, rhs: BeatPosition) -> Self::Output {
+        (self.0 - rhs.0).into()
+    }
+}
+
+impl Sub<&BeatPosition> for &BeatPosition {
+    type Output = BeatLength;
+
+    fn sub(self, rhs: &BeatPosition) -> Self::Output {
+        self.clone() - rhs.clone()
+    }
+}
 
 #[derive(Clone, Default, Data)]
 pub struct Score {
@@ -35,7 +126,7 @@ pub fn iterate_elements<'a>(
 ) -> impl Iterator<Item = (usize, usize)> + 'a {
     use ScoreElementKind::*;
     let mut elements = elements.enumerate().peekable();
-    std::iter::from_fn(move || {
+    iter::from_fn(move || {
         let (i, _) = elements.find(|(_, e)| matches!(e.borrow().kind, Start))?;
         let j = match elements
             .peeking_take_while(|(_, e)| matches!(e.borrow().kind, Continued))
@@ -48,10 +139,44 @@ pub fn iterate_elements<'a>(
     })
 }
 
+pub fn iterate_measures<'a>(
+    measures: &'a OrdMap<BeatPosition, BeatLength>,
+) -> impl Iterator<Item = (BeatPosition, BeatPosition)> + 'a {
+    let mut measure_lengths = measures.iter().peekable();
+    let mut measure_length = BeatLength::from(BigRational::from_integer(4.into()));
+    let mut measure_start_beat = BeatPosition::zero();
+
+    iter::from_fn(move || {
+        let mut measure_end_beat = &measure_start_beat + &measure_length;
+        if let Some((next_measure_beat, next_measure_length)) = measure_lengths.peek() {
+            if next_measure_beat == &&measure_start_beat {
+                measure_length = next_measure_length.clone().to_owned();
+                measure_end_beat = &measure_start_beat + &measure_length;
+                measure_lengths.next();
+            } else if next_measure_beat < &&measure_end_beat {
+                measure_length = next_measure_length.clone().to_owned();
+                measure_end_beat = next_measure_beat.clone().to_owned();
+                measure_lengths.next();
+            }
+        }
+        let ret = Some((measure_start_beat.clone(), measure_end_beat.clone()));
+        measure_start_beat = measure_end_beat;
+        ret
+    })
+}
+
 #[cfg(test)]
 mod test {
-    use super::{iterate_elements, ScoreElement, ScoreElementKind};
+    use super::iterate_elements;
+    use super::iterate_measures;
+    use super::BeatLength;
+    use super::BeatPosition;
+    use super::ScoreElement;
+    use super::ScoreElementKind;
+    use druid::im::ordmap;
+    use itertools::iterate;
     use itertools::Itertools;
+    use num::BigRational;
 
     #[test]
     fn test_iterate_elements() {
@@ -66,5 +191,31 @@ mod test {
             iterate_elements(elements).collect_vec(),
             vec![(0, 3), (3, 6), (8, 9), (10, 11),]
         );
+    }
+
+    #[test]
+    fn test_iterate_measures_01() {
+        let measures = ordmap![];
+        let got = iterate_measures(&measures).take(10).collect_vec();
+        let expected = iterate(0, |x| x + 4)
+            .map(|x| BeatPosition::from(BigRational::from_integer(x.into())))
+            .tuple_windows::<(_, _)>()
+            .take(10)
+            .collect_vec();
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn test_iterate_measures_02() {
+        let measures = ordmap![
+            BeatPosition::from(BigRational::from_integer(16.into())) => BeatLength::from(BigRational::from_integer(3.into()))
+        ];
+        let got = iterate_measures(&measures).take(10).collect_vec();
+        let expected = vec![0, 4, 8, 12, 16, 19, 22, 25, 28, 31, 34]
+            .into_iter()
+            .map(|x| BeatPosition::from(BigRational::from_integer(x.into())))
+            .tuple_windows::<(_, _)>()
+            .collect_vec();
+        assert_eq!(got, expected);
     }
 }
