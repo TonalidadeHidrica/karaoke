17ead9b89f43041402005961eb4fae464bf033b9
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-03-11 17:33:46 +0900

    Change curosr step

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use druid::WidgetPod;
 use druid::keyboard_types::Key;
 use druid::kurbo::Line;
+use druid::widget::Flex;
+use druid::widget::Label;
 use druid::AppLauncher;
 use druid::Color;
+use druid::Data;
 use druid::Event;
 use druid::Insets;
 use druid::KeyEvent;
@@ -20,7 +24,6 @@ use karaoke::schema::ScoreElement;
 use karaoke::schema::ScoreElementKind;
 use num::BigRational;
 use num::ToPrimitive;
-use num::Zero;
 use thiserror::Error;
 
 fn main() -> Result<(), EditorError> {
@@ -39,14 +42,36 @@ pub enum EditorError {
 
 struct ScoreEditor {
     cursor_position: BeatPosition,
+    cursor_delta: BeatLength,
+
+    // status_bar: WidgetPod<StatusBarData, Box<dyn Widget<StatusBarData>>>,
 }
 
 impl Default for ScoreEditor {
     fn default() -> Self {
         ScoreEditor {
             cursor_position: BeatPosition::zero(),
+            cursor_delta: BeatLength::one(),
+
+            // status_bar: WidgetPod::new(Box::new(build_status_bar())),
         }
     }
+}
+
+// #[derive(Clone, Data)]
+// struct StatusBarData {
+//     cursor_position: String,
+//     cursor_delta: String,
+// }
+// 
+// fn build_status_bar() -> impl Widget<StatusBarData> {
+//     Flex::row().with_child(Label::new("Hoge"))
+// }
+
+fn cursor_delta_candidates() -> impl DoubleEndedIterator<Item = BeatLength> {
+    vec![4, 8, 12, 16, 24, 32]
+        .into_iter()
+        .map(|x| BeatLength::from(BigRational::new(4.into(), x.into())))
 }
 
 impl Widget<Score> for ScoreEditor {
@@ -88,15 +113,31 @@ impl Widget<Score> for ScoreEditor {
                     ctx.request_paint();
                 }
                 Key::ArrowLeft => {
-                    self.cursor_position -= BeatLength::one();
+                    self.cursor_position -= &self.cursor_delta;
                     if self.cursor_position < BeatPosition::zero() {
                         self.cursor_position = BeatPosition::zero();
                     }
                     ctx.request_paint();
                 }
                 Key::ArrowRight => {
-                    self.cursor_position += BeatLength::one();
+                    self.cursor_position += &self.cursor_delta;
                     ctx.request_paint();
+                }
+                Key::ArrowUp => {
+                    let mut it = cursor_delta_candidates();
+                    let first = it.next().unwrap();
+                    self.cursor_delta = it
+                        .take_while(|x| x > &self.cursor_delta)
+                        .last()
+                        .unwrap_or(first);
+                }
+                Key::ArrowDown => {
+                    let mut it = cursor_delta_candidates().rev();
+                    let first = it.next().unwrap();
+                    self.cursor_delta = it
+                        .take_while(|x| x < &self.cursor_delta)
+                        .last()
+                        .unwrap_or(first);
                 }
                 _ => {}
             },
@@ -133,19 +174,28 @@ impl Widget<Score> for ScoreEditor {
         bc.max()
     }
 
-    #[allow(unreachable_code)]
     fn paint(&mut self, ctx: &mut druid::PaintCtx, data: &Score, _env: &druid::Env) {
         let insets = Insets::uniform(-8.0);
-        let rect = ctx.size().to_rect().inset(insets);
+        let status_bar_height = 24.0;
+        let status_bar_inset = Insets::new(0.0, -status_bar_height, 0.0, 0.0);
+        let draw_rect = ctx.size().to_rect().inset(insets).inset(status_bar_inset);
         let beat_width = 60.0;
         let line_height = 15.0;
         let note_height = 12.0;
         let line_margin = 5.0;
 
+        let rect = {
+            let mut r = ctx.size();
+            r.height = status_bar_height;
+            r.to_rect()
+        };
+        ctx.fill(&rect, &Color::BLACK);
+
         {
-            let mut y = rect.min_y();
+            let mut y = draw_rect.min_y();
             let mut left_beat: BeatPosition = BeatPosition::zero();
-            let get_x = |length: BeatLength| rect.min_x() + length.0.to_f64().unwrap() * beat_width;
+            let get_x =
+                |length: BeatLength| draw_rect.min_x() + length.0.to_f64().unwrap() * beat_width;
             for (beat_start, beat_end) in iterate_measures(&data.measure_lengths) {
                 if &beat_end - &left_beat > BeatLength::from(BigRational::from_integer(16.into())) {
                     let x = get_x(&beat_start - &left_beat);
@@ -172,7 +222,6 @@ impl Widget<Score> for ScoreEditor {
                 }
 
                 if (&beat_start..&beat_end).contains(&&self.cursor_position) {
-                    dbg!(&self.cursor_position, &left_beat);
                     let x = get_x(&self.cursor_position - &left_beat);
                     let line = Line::new((x, y), (x, y + line_height));
                     ctx.stroke(line, &Color::GREEN, 3.0);
@@ -188,8 +237,8 @@ impl Widget<Score> for ScoreEditor {
             let rect = Size::new((j - i) as f64 * beat_width, note_height)
                 .to_rect()
                 .with_origin((
-                    rect.min_x() + beat_width * i as f64,
-                    rect.min_y() + line_height - note_height,
+                    draw_rect.min_x() + beat_width * i as f64,
+                    draw_rect.min_y() + line_height - note_height,
                 ))
                 .to_rounded_rect(3.0);
             ctx.fill(rect, &Color::OLIVE);
